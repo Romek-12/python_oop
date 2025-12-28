30fe96a08032a5b5527aafd425517741a35adc40
Romek-12 <kemor.ikcindur@gmail.com>
2025-12-28 16:36:10 +0100

    Update index.py

--- 1_Car_RENTAL/index.py
+++ 1_Car_RENTAL/index.py
@@ -50,13 +50,33 @@ moje_biuro.add_vehicle(auto1)
 moje_biuro.add_vehicle(auto2)
 moje_biuro.add_vehicle(auto3)
 
+Bike1 = Bike("Giant", "Talon 1", 5)
+Bike2 = Bike("Trek", "Procaliber 6", 10)
+Bike3 = Bike("Trek", "Domane AL4", 15)
+
+moje_biuro.add_vehicle(Bike1)
+moje_biuro.add_vehicle(Bike2)
+moje_biuro.add_vehicle(Bike3)
+
+Scooter1 = Scooter("Vespa", "Szybka", 20)
+Scooter2 = Scooter("Vespa", "Wygodna", 18)
+Scooter3 = Scooter("Vespa", "Wolna", 16)
+
+moje_biuro.add_vehicle(Scooter1)
+moje_biuro.add_vehicle(Scooter2)
+moje_biuro.add_vehicle(Scooter3)
+
 print("Dostępne pojazdy:")
 for p in moje_biuro.vehicles:
     print(p)
 
 print("\nWypożyczam Ferrari na 6 dni")
 wynajem = moje_biuro.rent_vehicle(auto3, 6)
-print(f"Wynajęty samochód: {wynajem.vehicle.brand} na {wynajem.days} dni")
+print(f"Wynajęty samochód: {wynajem.vehicle.brand} na {wynajem.days} dni")  
+
+print("\nWypożyczam domane AL4 na 6 dni")
+wynajem = moje_biuro.rent_vehicle(Bike3, 6)
+print(f"Wynajęty rower: {wynajem.vehicle.brand} na {wynajem.days} dni")
 
 print(f"Ilość aktywnych wynajmów: {len(moje_biuro.rentals)}")
 
